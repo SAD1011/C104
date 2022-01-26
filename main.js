5449061330b36b33c87e1f71c06972bf4c61f739
SAD1011 <77442071+SAD1011@users.noreply.github.com>
2022-01-26 15:25:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+Webcam.attach( '#camera' );
+camera= document.getElementById("camera");
+
+    Webcam.set({
+        width:350,
+        height:300,
+        image_format: 'png',
+        png_quality:90
+    });
+
+function takesnapshot() 
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('m15 version:', m15.version);
+classifier = m15.imageClassifier('https://teachablemachine.withgoogle.com/models/ycN3yGckV/model.json', modelLoaded);
